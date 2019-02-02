fcac058927b7524fa8dcd235bfef945994cc9b37
Hamanor <rdomanoel@gmail.com>
2019-02-02 00:59:25 -0200

    remove api

--- utils/api.js
+++ utils/api.js
@@ -29,3 +29,10 @@ export const editCard = (deckKey, questionKey, question) => AsyncStorage.getItem
     data[deckKey].questions[questionKey] = question;
     AsyncStorage.setItem(DECK_STORAGE_KEY, JSON.stringify(data));
   });
+
+export const removeDeck = deckKey => AsyncStorage.getItem(DECK_STORAGE_KEY)
+  .then(JSON.parse)
+  .then((data) => {
+    const deleteDeck = data.filter(i => i.index !== deckKey);
+    AsyncStorage.setItem(DECK_STORAGE_KEY, JSON.stringify(deleteDeck));
+  });
